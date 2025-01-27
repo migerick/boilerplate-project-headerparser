a555a10f2e215636837d297ba0c7db2cbce5e081
Miguel Flores <47280537+migerick@users.noreply.github.com>
2025-01-27 12:23:22 -0500

    feat: Request Header Parser Microservice

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip
+  const language = req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0] : 'en'; // Default to 'en' if no language is provided
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
